5b66357d3ee5e6c7f9153ebe6268105bf3172a95
imoliviant <112634843+imoliviant@users.noreply.github.com>
2022-10-03 19:15:43 -0700

    commit

--- functions.js
+++ functions.js
@@ -2,9 +2,9 @@
 function mintScribble() {
     var amount1 = $("#amount1").val();
     var content = "Sending transaction from: ";
-    content += zombieMaster;
+    content += scribbleChef;
     $("#lang1").html(content);
-    var event = contractScribble.methods.batchMint(zombieMaster, amount1).send({ from: zombieMaster, value: 15000000000000000000 * amount1 })
+    var event = contractScribble.methods.batchMint(scribbleChef, amount1).send({ from: scribbleChef, value: 15000000000000000000 * amount1 })
         .then(function (receipt) {
             console.log(receipt);
     var content = "Transaction sent!: ";
@@ -16,7 +16,7 @@ function mintScribble() {
 
 function supply() {
     var content = "";
-    var event = contractScribble.methods.totalSupply().call({ from: zombieMaster })
+    var event = contractScribble.methods.totalSupply().call({ from: scribbleChef })
         .then(function (result) {
             console.log(result);
     var content = "Already minted: ";
@@ -28,11 +28,11 @@ function supply() {
 // Wallet
 function wallet() {
     var content = "";
-        var event = contractDoodle.methods.balanceOf(zombieMaster).call({ from: zombieMaster })
+        var event = contractDoodle.methods.balanceOf(scribbleChef).call({ from: scribbleChef })
         .then(function (result) {
     balance = result;
     for(var i = 0; i < balance; i++){
-    var event = contractDoodle.methods.tokenOfOwnerByIndex(zombieMaster, i).call({ from: zombieMaster })
+    var event = contractDoodle.methods.tokenOfOwnerByIndex(scribbleChef, i).call({ from: scribbleChef })
         .then(function (result) {
     var event = contractDoodle.methods.tokenURI(Number(result)).call()
         .then(function (result1) {
@@ -48,9 +48,9 @@ function sendNFT() {
     var address1 = $("#address1").val();
     var tokenId1 = $("#tokenId1").val();
     var content = "Sending transaction from: ";
-    content += zombieMaster;
+    content += scribbleChef;
     $("#lang4").html(content);
-    var event = contractScribble.methods.transferFrom(zombieMaster, address1, tokenId1).send({ from: zombieMaster })
+    var event = contractScribble.methods.transferFrom(scribbleChef, address1, tokenId1).send({ from: scribbleChef })
         .then(function (receipt) {
             console.log(receipt);
     var content = "Transaction sent!: ";
@@ -64,9 +64,9 @@ function sendNFT() {
 function approveS() {
     var tokenId2 = $("#tokenId2").val();
     var content = "Approving transaction from: ";
-    content += zombieMaster;
+    content += scribbleChef;
     $("#lang5").html(content);
-    var event = contractScribbleStake.methods.approve("0xf77DBC9d03428980db5EF78D5d9C3D6a55F29829", tokenId2).send({ from: zombieMaster })
+    var event = contractScribbleStake.methods.approve("nft-ca", tokenId2).send({ from: scribbleChef })
         .then(function (receipt) {
             console.log(receipt);
     var content = "Approved!: ";
@@ -79,9 +79,9 @@ function approveS() {
 function stakeNFT() {
     var tokenId2 = $("#tokenId2").val();
     var content = "Sending transaction from: ";
-    content += zombieMaster;
+    content += scribbleChef;
     $("#lang6").html(content);
-    var event = contractScribbleStake.methods.stake(tokenId2).send({ from: zombieMaster, value: 5000000000000000 })
+    var event = contractScribbleStake.methods.stake(tokenId2).send({ from: scribbleChef, value: 5000000000000000 })
         .then(function (receipt) {
             console.log(receipt);
     var content = "Transaction sent!: ";
@@ -105,9 +105,9 @@ function calculateReward() {
 function unstakeNFT() {
     var tokenId4 = $("#tokenId4").val();
     var content = "Sending transaction from: ";
-    content += zombieMaster;
+    content += scribbleChef;
     $("#lang8").html(content);
-    var event = contractScribbleStake.methods.unstake(tokenId4).send({ from: zombieMaster, value: 5000000000000000 })
+    var event = contractScribbleStake.methods.unstake(tokenId4).send({ from: scribbleChef, value: 5000000000000000 })
         .then(function (receipt) {
             console.log(receipt);
     var content = "Transaction sent! ";
